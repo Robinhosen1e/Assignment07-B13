0a174f736a1ac4163969a2f93dad31e079731d28
Robin Hosen <nikiaafri@gmail.com>
2026-04-30 13:23:58 +0600

    setup project

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-amber-300 text-xl">daisyUI</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
