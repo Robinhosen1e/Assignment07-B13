0ba6f3e9e6cca9b978f0e914a917bd1b32fd8205
Robin Hosen <nikiaafri@gmail.com>
2026-05-04 00:01:26 +0600

    fixt error

--- src/app/friends/page.jsx
+++ src/app/friends/page.jsx
@@ -16,14 +16,14 @@ const Friend = ({ friend }) => {
             <div  className='p-4  justify-items-center  bg-white border border-gray-100 rounded-2xl shadow-sm hover:shadow-xl transition'>
                 
                         <Image 
-                        src={friend.picture}
-                         alt={friend.name} 
+                        src={friend?.picture}
+                         alt={friend?.name} 
                          width={100} 
                          height={100}  
                          className='rounded-full mb-3' />
                
-                    <h3 className='text-xl text-[#1F2937] font-semibold mb-1.5'>{friend.name}</h3>
-                    <p className='text-[#64748B] text-xs mb-2.5'>{friend.days_since_contact} ago</p>
+                    <h3 className='text-xl text-[#1F2937] font-semibold mb-1.5'>{friend?.name}</h3>
+                    <p className='text-[#64748B] text-xs mb-2.5'>{friend?.days_since_contact} ago</p>
                     <div className='items-center mb-2.5'>
                         {friend.tags.map((tag, index) => (
                         <span key={`${tag}-${index}`} className='inline-block  bg-[#CBFADB] rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
@@ -32,14 +32,14 @@ const Friend = ({ friend }) => {
                     ))}
                     </div>
                     <div className='items-center mb-3'>
-                        <span className={`inline-block px-3 py-1 text-sm font-semibold text-white rounded-full ${getStatusClass(friend.status)}`}>
-                         {friend.status}
+                        <span className={`inline-block px-3 py-1 text-sm font-semibold text-white rounded-full ${getStatusClass(friend?.status)}`}>
+                         {friend?.status}
                     </span>
                     </div>
  
                     <div className='w-full mt-6'>
                         <button className='block text-center w-full text-xs text-gray-500 border border-gray-200 rounded-lg py-1.5 hover:bg-gray-50 transition-colors'>
-                            <Link href={`/friends/${friend.id}`} className=' flex items-center justify-center gap-2'>
+                            <Link href={`/friends/${friend?.id}`} className=' flex items-center justify-center gap-2'>
                             View Details <MoveRight size={15} />
                         </Link>
                         </button>
--- src/component/HomePage/Friends/Friends.jsx
+++ src/component/HomePage/Friends/Friends.jsx
@@ -12,7 +12,7 @@ const Friends = () => {
                 <h1 className='tex-[#1F2937] font-semibold text-2xl pt-14 mb-4 border-t-2 border-gray-200'>Your Friends</h1>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4'>
-            {friends.map((friend) => <Friend key={friend.id} friend={friend} />)}
+            {friends?.map((friend) => <Friend key={friend.id} friend={friend} />)}
         </div>
         </div>
     );
